e85ca9f23dcd2b082381e08a518a7be98ed112b3
johnnychang25678 <zjps3407@gmail.com>
2024-11-23 21:01:54 -0800

    add some code for quick experiments

--- common/hyperparams.py
+++ common/hyperparams.py
@@ -1,6 +1,6 @@
 
 class HyperParams:
     MaxStateVisitCount = 5
-    MaxNumConversationRounds = 100
+    MaxNumConversationRounds = 70
     DefaultTemperature = 1
     DefaultMaxTokens = 2000
--- run_tot.py
+++ run_tot.py
@@ -1,27 +1,52 @@
 import sys
 from common.config import Config
 from tot.tot import TreeOfThought
+import json
+import argparse
 
 #
 # Example Sudoku problems:
 # '[[*, 3, 1], [*, 2, 3], [3, *, 2]]'
 # '[[1, *, *, 2], [*, 1, *, 4], [*, 2, *, *], [*, *, 4, *]]'
 #
+
+
 if __name__ == "__main__":
-    if not len(sys.argv) == 2:
-        print("""Usage:""")
-        print("""    python run_tot.py "<problem_description>" """)
-        print("""Example:""")
-        print("""    python run_tot.py "please solve this 3x3 sudoku puzzle [[*, 3, 1], [*, 2, 3], [3, *, 2]] where * represents a cell to be filled in." """)
-        print("""    python run_tot.py "please solve this 4x4 sudoku puzzle [[*,1,*,*],[*,*,2,*],[*,*,*,4],[1,*,*,*]] where * represents a cell to be filled in." """)
-        exit(1)
-    
-    user_input = sys.argv[1]
+
+    # Initialize parser
+    parser = argparse.ArgumentParser()
+
+    # Add arguments
+    parser.add_argument("-data", required=True, type=str, help="data path")
+
+    # Parse arguments
+    args = parser.parse_args()
+    # print(args.data)
+    # Use arguments
+    # if args
+    dimension = args.data.split("/")[-1][:3]
     path_to_config_yaml = "./config.yaml"
     config = Config(path_to_config_yaml)
     tot = TreeOfThought(config)
-
-    success, solution = tot.run(user_input)
-    print("")
-    print("Success :", success)
-    print("Solution:", solution)
+    initial_prompt = f"please solve this {dimension} sudoku puzzle"
+    try:
+        with open(args.data) as f:
+            sudokus = json.load(f)
+            for sudoku in sudokus:
+                prompt = initial_prompt + " " + sudoku + " " + \
+                    "where * represents a cell to be filled in."
+                # print("ppp", prompt)
+                success, solution = tot.run(prompt)
+                print("")
+                print("Success :", success)
+                print("Solution:", solution)
+    except FileNotFoundError:
+        print(f"Error: The file '{args.data}' was not found.")
+        sys.exit(1)  # Exit the program with a non-zero status code
+    except json.JSONDecodeError:
+        print(
+            f"Error: Failed to parse JSON from the file '{args.data}'. Please check the file format.")
+        sys.exit(1)
+    except IOError as e:
+        print(f"Error: An I/O error occurred: {e}")
+        sys.exit(1)
